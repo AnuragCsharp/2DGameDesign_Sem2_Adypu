01e78dba7bcb0f715ddb0e52a92b46853ecbb198
AnuragCsharp <77142556+AnuragCsharp@users.noreply.github.com>
2021-05-06 13:10:12 +0530

    Added The Score and Audio
    Checked !

--- 2DGameDesign_Class/Assets/02_BubblePopper/Scripts/BubblePopper.cs
+++ 2DGameDesign_Class/Assets/02_BubblePopper/Scripts/BubblePopper.cs
@@ -1,25 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BubblePopper : MonoBehaviour
 {
 	Animator anim;
 
+	AudioSource Audio;
+
+	ScoreKeeper ScoreHandler;
+
+	public TMP_Text ScoreText;
 
 	private void Start()
 	{
 		anim = this.GetComponent<Animator>();
+		Audio = this.GetComponent<AudioSource>();
+		ScoreHandler = FindObjectOfType<ScoreKeeper>();
+		
 		
 	}
 
 	private void OnMouseDown()
 	{
+		
+		ScoreHandler.Score++;
 
+		ScoreText.text = ScoreHandler.Score.ToString();
 		//Debug.Log("On Mouse down is working");
 		anim.SetBool("Pop", true);
 
 		Invoke("DestroytheBubble", 0.6f);
+
+		Audio.Play();
 
 
 	}
